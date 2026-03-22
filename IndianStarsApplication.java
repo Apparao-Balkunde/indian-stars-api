3614675366cc5d80a7c0fea99e53a17041b2b14b
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2026-03-22 18:01:08 +0530

    Add files via upload

---
+++ IndianStarsApplication.java
@@ -0,0 +1,11 @@
+package com.indianstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianStarsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IndianStarsApplication.class, args);
+    }
+}
